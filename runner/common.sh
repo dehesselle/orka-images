578d60fcd3971fbf790f3ff46679ee2c25648890
René de Hesselle <dehesselle@web.de>
2026-05-14 19:30:29 +0200

    Update ccache to 4.13.6

--- runner/common.sh
+++ runner/common.sh
@@ -212,7 +212,7 @@ function install_ccache
 {
   echo -e "$ANSI_FG_YELLOW_BRIGHT${FUNCNAME[0]}$ANSI_FG_RESET"
 
-  local ccache_ver=${1:-4.12.2}
+  local ccache_ver=${1:-4.13.6}
   local ccache_url=https://github.com/ccache/ccache/releases/download/v$ccache_ver/ccache-$ccache_ver-darwin.tar.gz
 
   _mkdir /opt/ccache
